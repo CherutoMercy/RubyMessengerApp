1259b5bcfe70b34cfea1b3f6e7c2fd35860dec68
CherutoMercy <m.thirty100@gmail.com>
2017-03-03 18:13:27 +0300

    add controllers

--- MessengerApp/app/controllers/messages_controller.rb
+++ MessengerApp/app/controllers/messages_controller.rb
@@ -2,4 +2,22 @@ class MessagesController < ApplicationController
 	def index
 		@messages = Message.all
 	end
+
+	def new
+		@message = Message.new
+	end
+
+	def create
+		@message =Message.new(messages_params)
+		if @message.save
+			redirect_to '/messages'
+		else
+			render'new'
+		end
+	end
+
+	private
+	def messages_params
+		params.require(:message).permit(:content)
+	end
 end
