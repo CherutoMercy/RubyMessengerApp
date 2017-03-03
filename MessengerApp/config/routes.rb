5811dd7e80473cf0eae5a884279c85df20880cf9
CherutoMercy <m.thirty100@gmail.com>
2017-03-03 18:14:41 +0300

    add message routes

--- MessengerApp/config/routes.rb
+++ MessengerApp/config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-	get 'messages' => 'messages#index'
+	get 'messages'     => 'messages#index'
+	get 'messages/new' => 'messages#new'
+	def new 
+		@messages = Message.new
+	end
+	post 'messages' => 'messages#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
